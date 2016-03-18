43d18ea779646b860ffd7c134290f5fc7102111f
0x4849 <bpharris+secondgithub@ualberta.ca>
2016-03-18 11:13:36 -0600

    Added Camera Exercise...

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -0,0 +1,109 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+public class LonelyTwitterActivity extends Activity {
+
+    private EditText bodyText;
+    private ListView oldTweetsList;
+
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+    private ArrayAdapter<Tweet> adapter;
+
+    private Button saveButton;
+
+    private ImageButton pictureButton;
+    private Bitmap thumbnail;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1234;
+
+    public ArrayAdapter<Tweet> getAdapter() {
+        return adapter;
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        bodyText = (EditText) findViewById(R.id.tweetMessage);
+        oldTweetsList = (ListView) findViewById(R.id.tweetsList);
+
+
+        pictureButton = (ImageButton) findViewById(R.id.pictureButton);
+        pictureButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                String mbodyText = bodyText.getText().toString();
+                NormalTweet myTweet = new NormalTweet(mbodyText);
+                tweets.add(0, myTweet);
+                myTweet.addThumbnail(thumbnail);
+                adapter.notifyDataSetChanged();
+                ElasticsearchTweetController.AddTweetTask newTask = new ElasticsearchTweetController.AddTweetTask();
+                newTask.execute(myTweet);
+                pictureButton.setImageResource(android.R.color.transparent);
+                thumbnail = null;
+                setResult(RESULT_OK);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Get the latest tweets from Elasticsearch
+        ElasticsearchTweetController.GetTweetsTask getTweetsTask = new ElasticsearchTweetController.GetTweetsTask();
+//        getTweetsTask.execute("test");
+        getTweetsTask.execute("");
+        try {
+            tweets = new ArrayList<Tweet>();
+            tweets.addAll(getTweetsTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // Binds tweet list with view, so when our array updates, the view updates with it
+        adapter = new TweetAdapter(this, tweets); /* NEW! */
+        oldTweetsList.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data .getExtras();
+            thumbnail = (Bitmap) extras.get("data");
+            pictureButton.setImageBitmap(thumbnail);
+        }
+    }
+
+}
